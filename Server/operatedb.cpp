e4abfd6a77991cfd491c77e82a2e57ce52c1cdab
stark-lbx <2396573637@qq.com>
2025-06-25 21:57:31 +0800

    解决SQL注入后的版本（使用Qt的QSqlQuery对象的prepare和bindVal实现）

--- Server/operatedb.cpp
+++ Server/operatedb.cpp
@@ -12,7 +12,7 @@ OperateDB &OperateDB::getInstance()
 
 OperateDB::~OperateDB()
 {
-    m_db.close();
+    m_db.close();//采用sql连接池管理时无需手动关闭
 }
 
 OperateDB::OperateDB(QObject *parent)
@@ -51,19 +51,24 @@ int OperateDB::handleRegist(const char *uid, const char *pwd)
     if(uid==nullptr || pwd==nullptr){
         return 0;
     }
-    QString sqlStr;//sql语句
-    QSqlQuery q;//执行对象
+    // QString sqlStr;//sql语句
+    // sqlStr = QString("select * from user_info where name = '%1';").arg(uid);
 
-    sqlStr = QString("select * from user_info where name = '%1';").arg(uid);
-    if(!q.exec(sqlStr)){
+    QSqlQuery q(m_db);//执行对象
+    q.prepare("select * from user_info where name = :name");
+    q.bindValue(":name",QString::fromUtf8(uid));
+    if(!q.exec()){
         return 0;//QString("sql execute error(select)");
     }
     if(q.next()){
         return -1;//QString("name have already existed");
     }
 
-    sqlStr = QString("insert into user_info(name,pwd) values('%1','%2');").arg(uid,pwd);
-    if(!q.exec(sqlStr)){
+    // sqlStr = QString("insert into user_info(name,pwd) values('%1','%2');").arg(uid,pwd);
+    q.prepare("insert  into user_info(name,pwd) values(:name, :pwd)");
+    q.bindValue(":name",QString::fromUtf8(uid));
+    q.bindValue(":pwd",QString::fromUtf8(pwd));
+    if(!q.exec()){
         return 0;//QString("sql execute error(insert)");
     }
     return 1;//QString("register successfully");
@@ -74,10 +79,15 @@ int OperateDB::handleLogin(const char *uid, const char *pwd)
     if(uid==nullptr || pwd==nullptr){
         return 0;//QString("Illegal empty content");
     }
-    QString sqlStr;//sql语句
-    QSqlQuery q;//执行对象
-    sqlStr = QString("select * from user_info where name = '%1' and pwd = '%2';").arg(uid,pwd);
-    if(!q.exec(sqlStr)){
+
+    // QString sqlStr;//sql语句
+    // sqlStr = QString("select * from user_info where name = '%1' and pwd = '%2';").arg(uid,pwd);
+
+    QSqlQuery q(m_db);//执行对象
+    q.prepare("select * from user_info where name = :name and pwd = :pwd");
+    q.bindValue(":name",QString::fromUtf8(uid));
+    q.bindValue(":pwd",QString::fromUtf8(pwd));
+    if(!q.exec()){
         return 0;//QString("sql execute error(select)");
     }
     if(!q.next()){
@@ -86,8 +96,10 @@ int OperateDB::handleLogin(const char *uid, const char *pwd)
 
 
     //修改登录在线状态
-    sqlStr = QString("update user_info set online = '1' where name = '%1';").arg(uid);
-    if(!q.exec(sqlStr)){
+    // sqlStr = QString("update user_info set online = '1' where name = '%1';").arg(uid);
+    q.prepare("update user_info set online = '1' where name = :name");
+    q.bindValue(":name", QString::fromUtf8(uid));
+    if(!q.exec()){
         return 0;//QString("sql excute error(update)");
     }
     return 1;//QString("login successfully");
@@ -97,11 +109,14 @@ int OperateDB::handleFindUser(const char *uid)
 {
     if(uid==nullptr) return 0;//"查找失败";
 
-    QString sqlStr = QString("select online from user_info where name='%1'").arg(uid);
+    // QString sqlStr = QString("select online from user_info where name='%1'").arg(uid);
     //qDebug() <<"sql: 查询用户是否在线"<<sqlStr;
 
-    QSqlQuery q;
-    if(!q.exec(sqlStr)){
+    QSqlQuery q(m_db);
+
+    q.prepare("select online from user_info where name = :name");
+    q.bindValue(":name", QString::fromUtf8(uid));
+    if(!q.exec()){
         return 0;//"查找失败";
     }
     if(q.next()){
@@ -114,57 +129,71 @@ int OperateDB::handleFindUser(const char *uid)
 
 int OperateDB::handleAddFriend(const char *curName, const char *tarName)
 {
-    if(curName==nullptr || tarName==nullptr)return 0;
-    if(QString(curName)==QString(tarName))return -1;//不可添加自己为好友
+    if(curName==nullptr || tarName==nullptr) return 0;
+    if(QString(curName) == QString(tarName)) return -1;
 
-    QString sqlStr = QString(R"(
-            select * from friend
-            where(
-                user_id = (select id from user_info where name='%1')
-                and
-                friend_id = (select id from user_info where name='%2')
-            ) or (
-                user_id = (select id from user_info where name='%2')
-                and
-                friend_id = (select id from user_info where name='%1')
-            )
-            )").arg(curName,tarName);
 
-    // qDebug()<<"OperateDB(handleAddFriend):"
-    //          <<"\t sql:查询是否是好友"<<sqlStr;
+    QSqlQuery q(m_db);
+    // 预处理：检查是否已是好友
+    q.prepare(R"(
+        SELECT * FROM friend
+        WHERE (
+            user_id = (SELECT id FROM user_info WHERE name = :curName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :tarName)
+        ) OR (
+            user_id = (SELECT id FROM user_info WHERE name = :tarName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :curName)
+        )
+    )");
+    q.bindValue(":curName", QString::fromUtf8(curName));
+    q.bindValue(":tarName", QString::fromUtf8(tarName));
+    if(!q.exec()) return 0;
+    if(q.next()) return -1; // 已是好友
 
-    QSqlQuery q;
-    if(!q.exec(sqlStr))return 0;//执行sql失败
-    if(q.next())return -1;//对方已是您的好友
+    // 预处理：检查目标用户是否在线
+    q.prepare("SELECT online FROM user_info WHERE name = :tarName");
+    q.bindValue(":tarName", QString::fromUtf8(tarName));
+    if(!q.exec()) return 0;
+    if(!q.next() || !q.value(0).toInt()) return -2; // 用户不存在或不在线
 
-    sqlStr = QString("select online from user_info where name='%1'").arg(tarName);//再次确认对方是否在线
-    if(!q.exec(sqlStr))return 0;//执行sql失败
-    if(!q.next() || !q.value(0).toInt())return -2;//用户不存在或者下线了
-
-    return 1;//添加请求成功
+    return 1; // 添加请求成功
 }
+
 
 bool OperateDB::handleAgreeAdd(const char *curName, const char *tarName)
 {
     if(curName==nullptr || tarName==nullptr)return 0;
-    QString sqlStr = QString(R"(
+    // QString sqlStr = QString(R"(
+    //     insert into friend(user_id,friend_id)
+    //         select u1.id, u2.id
+    //         from user_info u1, user_info u2
+    //         where (u1.name='%1' and u2.name='%2')
+    //             or (u1.name='%2' and u2.name='%1')
+    // )").arg(curName,tarName);
+
+    QSqlQuery q(m_db);
+    q.prepare(R"(
         insert into friend(user_id,friend_id)
             select u1.id, u2.id
             from user_info u1, user_info u2
-            where (u1.name='%1' and u2.name='%2')
-                or (u1.name='%2' and u2.name='%1')
-    )").arg(curName,tarName);
-
-    QSqlQuery q;
-    return q.exec(sqlStr);
+            where (u1.name= :fname and u2.name= :sname)
+                or (u1.name= :sname and u2.name= :fname)
+    )");
+    q.bindValue(":fname",QString::fromUtf8(curName));
+    q.bindValue(":sname",QString::fromUtf8(tarName));
+    return q.exec();
 }
 
 void OperateDB::handleOffline(const char *uid)
 {
     if(uid == nullptr)return;
-    QString sql = QString("update user_info set online=0 where name = '%1'").arg(uid);
+    // QString sql = QString("update user_info set online='0' where name = '%1'").arg(uid);
+    // QSqlQuery().exec(sql);
 
-    QSqlQuery().exec(sql);
+    QSqlQuery q(m_db);
+    q.prepare("update user_info set online='0' where name = :name");
+    q.bindValue(":name",QString::fromUtf8(uid));
+    q.exec();//无需返回值，离线操作允许失败
     qDebug()<<"sql: 用户"+QString(uid)+"已下线";
 }
 
@@ -172,57 +201,103 @@ QStringList OperateDB::handleGetFriendList(const char *uid)
 {
     QStringList result;
     if(uid==nullptr)return result;
-
-    QString sqlStr = QString(R"(
-                    select name from user_info
-                    where id in(
-                        select friend_id from friend where user_id =
-                        (select id from user_info where name='%1')
-                        union
-                        select user_id from friend where friend_id =
-                        (select id from user_info where name='%1')
-                    )
-                    )").arg(uid);
-    QSqlQuery q;
-    q.exec(sqlStr);
+    QSqlQuery q(m_db);
+    q.prepare(R"(
+                select name from user_info
+                where id in(
+                    select friend_id from friend where user_id =
+                    (select id from user_info where name= :name)
+                    union
+                    select user_id from friend where friend_id =
+                    (select id from user_info where name= :name)
+                )
+            )");
+    q.bindValue(":name",QString::fromUtf8(uid));
+    q.exec();
     while(q.next()){
         result.append(q.value(0).toString());
     }
     return result;
 }
 
-int OperateDB::handleDelFriend(const char *curName, const char *tarName)
+// int OperateDB::handleDelFriend(const char *curName, const char *tarName)
+// {
+//     if(curName==nullptr || tarName==nullptr)return 0;
+//     QString sqlStr = QString(R"(
+//             select * from friend
+//             where(
+//                 user_id = (select id from user_info where name='%1')
+//                 and
+//                 friend_id = (select id from user_info where name='%2')
+//             ) or (
+//                 user_id = (select id from user_info where name='%2')
+//                 and
+//                 friend_id = (select id from user_info where name='%1')
+//             )
+//             )").arg(curName,tarName);
+//     QSqlQuery q;
+//     if(!q.exec(sqlStr))return 0;//执行sql失败
+//     if(!q.next())return -1;//对方已不是你的好友
+
+//     sqlStr = QString(R"(
+//             delete from friend
+//             where(
+//                 user_id = (select id from user_info where name='%1')
+//                 and
+//                 friend_id = (select id from user_info where name='%2')
+//             ) or (
+//                 user_id = (select id from user_info where name='%2')
+//                 and
+//                 friend_id = (select id from user_info where name='%1')
+//             )
+//             )").arg(curName,tarName);
+
+//     if(!q.exec(sqlStr))return 0;//执行sql失败
+//     return 1;
+// }
+int OperateDB::handleDelFriend(const char* curName, const char* tarName)
 {
-    if(curName==nullptr || tarName==nullptr)return 0;
-    QString sqlStr = QString(R"(
-            select * from friend
-            where(
-                user_id = (select id from user_info where name='%1')
-                and
-                friend_id = (select id from user_info where name='%2')
-            ) or (
-                user_id = (select id from user_info where name='%2')
-                and
-                friend_id = (select id from user_info where name='%1')
-            )
-            )").arg(curName,tarName);
-    QSqlQuery q;
-    if(!q.exec(sqlStr))return 0;//执行sql失败
-    if(!q.next())return -1;//对方已不是你的好友
+    if (curName == nullptr || tarName == nullptr) return 0;
 
-    sqlStr = QString(R"(
-            delete from friend
-            where(
-                user_id = (select id from user_info where name='%1')
-                and
-                friend_id = (select id from user_info where name='%2')
-            ) or (
-                user_id = (select id from user_info where name='%2')
-                and
-                friend_id = (select id from user_info where name='%1')
-            )
-            )").arg(curName,tarName);
 
-    if(!q.exec(sqlStr))return 0;//执行sql失败
+    QSqlQuery q(m_db);
+
+    // 检查是否是好友
+    q.prepare(R"(
+        SELECT * FROM friend
+        WHERE (
+            user_id = (SELECT id FROM user_info WHERE name = :curName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :tarName)
+        ) OR (
+            user_id = (SELECT id FROM user_info WHERE name = :tarName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :curName)
+        )
+    )");
+    q.bindValue(":curName", QString::fromUtf8(curName));
+    q.bindValue(":tarName", QString::fromUtf8(tarName));
+    if (!q.exec()) {
+        return 0;
+    }
+    if (!q.next()) {
+        return -1; // 不是好友
+    }
+
+    // 删除好友关系
+    q.prepare(R"(
+        DELETE FROM friend
+        WHERE (
+            user_id = (SELECT id FROM user_info WHERE name = :curName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :tarName)
+        ) OR (
+            user_id = (SELECT id FROM user_info WHERE name = :tarName)
+            AND friend_id = (SELECT id FROM user_info WHERE name = :curName)
+        )
+    )");
+    q.bindValue(":curName", QString::fromUtf8(curName));
+    q.bindValue(":tarName", QString::fromUtf8(tarName));
+    if (!q.exec()) {
+        return 0;
+    }
+
     return 1;
 }
